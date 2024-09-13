3e23599a76093297cd43807a9700ab7bcab1173c
LucianoScaglione <lucianoscaglione21@gmail.com>
2024-09-13 14:34:54 -0300

    Redux integration

--- api/src/models/Publications.js
+++ api/src/models/Publications.js
@@ -5,7 +5,8 @@ module.exports = (sequelize) => {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      allowNull: false
+      allowNull: false,
+      autoIncrement: true
     },
     title: {
       type: DataTypes.STRING,
