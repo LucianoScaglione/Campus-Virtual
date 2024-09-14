0dbd65d761b46fdf05c93b257c776cf94576662c
g0770 <andresmoralesjulian@gmail.com>
2024-09-14 15:16:32 -0300

    home done

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,30 +7,22 @@ import React from 'react'
 import './Home.scss'
 
 import LastUpdates from './HomeContent/LastUpdates'
+import CalendarUpdates from './HomeContent/Calendar';
 
 const Home = () => {
+
   const dispatch = useDispatch();
   const curriculumUnit = useSelector(state => state.curriculumUnit);
   useEffect(() => {
     !curriculumUnit.length &&
       dispatch(getCurriculumUnit());
   }, [dispatch, curriculumUnit.length])
-  return (
-    <div>
-      {/*
-        curriculumUnit.length ? curriculumUnit.map(c => {
-          return (
-            <div key={c.id}>
-              <Link to={`${c.id}`}>
-                <h1>{c.name}</h1>
-              </Link>
-              <h3>{c.assignedTeacher}</h3>
-            </div>
-          );
-        }) : <h4>There are no registered curriculum unit</h4>
-      */}
 
-    <LastUpdates/>
+  return (
+    <div className='Home'>
+
+      <LastUpdates/>
+      <CalendarUpdates/>
 
     </div>
   )
--- client/src/components/HomeContent/LastUpdates.jsx
+++ client/src/components/HomeContent/LastUpdates.jsx
@@ -1,18 +1,44 @@
 import React from 'react'
 import '../Home.scss'
+import { Link } from 'react-router-dom';
 
-import UnitCurrCard from './UnitCurrCard';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurriculumUnit } from '../../redux/actions';
+
 
 
 function LastUpdates() {
 
-
+  const dispatch = useDispatch();
+  const curriculumUnit = useSelector(state => state.curriculumUnit);
+  useEffect(() => {
+    !curriculumUnit.length &&
+      dispatch(getCurriculumUnit());
+  }, [dispatch, curriculumUnit.length])
   
 
   return (
-    <div className="lastupdates">
+    <div className="CurrUnitContent">
       
+      <div className='CurrUnitHeader'>
+        <h2><b>Unidades Curriculares</b></h2>
+      </div>
 
+      {curriculumUnit.map((value,index) => {
+        return (
+          <Link to={`/curriculumUnit/${value.id}`}>
+            <div className='CurrUnitCard' key={value.id}>
+              <div className='CurrUnitTitle'><h2>{value.name}</h2></div>
+              <div className='CurrUnitInfo'>
+
+                    <span><b>Profesor asignado: </b>{value.assignedTeacher}</span>
+
+              </div>
+            </div>
+          </Link>
+        );
+      })}
 
     </div>
     
