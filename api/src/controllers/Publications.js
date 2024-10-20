0ab2fa1a3d8788edfb18f8e30374d7145a078e73
Ernesto Bessone <ejubessone@gmail.com>
2024-10-20 11:34:05 -0300

    get Publications added

--- api/src/controllers/Publications.js
+++ api/src/controllers/Publications.js
@@ -1,2 +1,13 @@
 const { Publications } = require('../db');
 
+const getPublications = async (req, res, next) => {
+    try {
+    const publications = await Publications.findAll({ include:  Users});
+    if (!publications.length) {
+    return res.status(404).send('There are no publications registered on the data base.');
+    }
+    res.status(200).json(publications);
+    } catch (error) {
+    next(error);
+    }
+};
