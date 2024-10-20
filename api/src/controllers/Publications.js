4a065345b4706a5f61df0d8785f432f549f366b4
Ernesto Bessone <ejubessone@gmail.com>
2024-10-20 12:09:29 -0300

    Find publication by ID added. constants renamed

--- api/src/controllers/Publications.js
+++ api/src/controllers/Publications.js
@@ -2,14 +2,30 @@ const { Publications } = require('../db');
 
 const getPublications = async (req, res, next) => {
     try {
-    const publications = await Publications.findAll({ include:  Users});
-    if (!publications.length) {
+    const findPublications = await Publications.findAll({ include:  Users});
+    if (!findPublications.length) {
     return res.status(404).send('There are no publications registered on the data base.');
     }
-    res.status(200).json(publications);
+    res.status(200).json(findPublications);
     } catch (error) {
     next(error);
     }
 };
 
-//
+const getPublicationById = async (req, res, next) => {
+    try {
+    const { id } = req.params;
+    const findPublicationById = await Publications.findByPk(id);
+    if (!findPublicationById) {
+        return res.status(404).send('There is no publication with that ID registered in the data base.');
+    }
+    res.status(200).json(findPublicationById);
+    } catch (error) {
+    next(error);
+    }
+};
+
+module.exports ={
+    getPublications,
+    getPublicationById
+}
